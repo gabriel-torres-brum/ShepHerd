aecfee4ae1d10e84cf561c71dc90c282e67dc3db
Gabriel Torres Brum <clustershope@gmail.com>
2022-05-02 16:04:39 -0300

    Alterações no layout do dashboard (aside, header e menus)

--- resources/views/app/dashboard.blade.php
+++ resources/views/app/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    <div class="flex flex-col items-center">
-        oi
+    <div class="flex flex-col items-center text-white">
+        <h1 class="text-3xl font-bold">TU É UM PUPILO!</h1>
     </div>
 @endsection
